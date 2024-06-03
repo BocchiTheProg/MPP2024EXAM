21dc65763183a33519c2af2614372f751c0faaca
BocchiTheProg <144540783+BocchiTheProg@users.noreply.github.com>
2024-06-03 16:36:17 +0300

    Add comments to middleware

--- app/middleware/api_request_logger.rb
+++ app/middleware/api_request_logger.rb
@@ -3,22 +3,27 @@ class ApiRequestLogger
     @app = app
   end
 
+  # Обов'язковий метод call для middleware
   def call(env)
+    # Отримання запиту через RACK
     request = Rack::Request.new(env)
     start_time = Time.now
-
+    # Отримання необхідної інформації для журналу
     status, headers, response = @app.call(env)
 
     duration = Time.now - start_time
     log_request(request, status, duration)
 
+    # Передаємо далі результати, іншим middleware або самому додатку
     [status, headers, response]
   end
 
   private
 
+  # Метод для додавання запису
   def log_request(request, status, duration)
     ApiRequestLog.create(
+      # Занесення необхідної інформації
       method: request.request_method,
       path: request.path,
       status: status,
